3c52a3c3f4c74197539212e137cf3a64a68c3ede
HPPC <2498525982@qq.com>
2019-09-15 22:39:12 +0800

    零售分析报表查询，修正部分bug

--- src/native/selecttab.vue
+++ src/native/selecttab.vue
@@ -48,7 +48,7 @@
             </div>
             <!-- 品类-->
             <div  :style="{'visibility':selectIndex==1?'visible':'hidden'}"  style="position: absolute;left: 20;top: 10;right: 10;bottom: 0">
-                <list style="flex: 1" >
+                <list style="flex: 1" v-if="!noDataFlag" >
                     <header>
                         <div class="banner">
                             <text class="txttitle" :style="{width:'150px'}">名次</text>
@@ -60,11 +60,11 @@
                     </header>
                     <cell v-for="(ls,index) in lists" :key="index" >
                         <div class="panel">
-                            <text class="text" :style="{width:'150px'}">{{ls.No}}</text>
+                            <text class="text" :style="{width:'150px'}">{{index+1}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Name}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Quantity}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Amount}}</text>
-                            <text class="text" :style="{width:'150px'}">{{ls.LvStr}}</text>
+                            <text class="text" :style="{width:'150px'}">{{ls.AmtLVStr}}</text>
                         </div>
                     </cell>
 
@@ -73,13 +73,17 @@
                         <loading-indicator class="indicator"></loading-indicator>
                         <text class="indicator-text">Loading...</text>
                     </loading>
-
                 </list>
+
+                <div v-if="noDataFlag" style="justify-content: center;align-items: center;height: 100">
+                    <text style="color: #000000;font-size: 30">暂无数据！</text>
+                </div>
+
             </div>
 
            <!-- 品牌-->
             <div  :style="{'visibility':selectIndex==2?'visible':'hidden'}"  style="position: absolute;left: 20;top: 10;right: 10;bottom: 0">
-                <list style="flex: 1" >
+                <list style="flex: 1" v-if="!noDataFlag">
                     <header>
                         <div class="banner">
                             <text class="txttitle" :style="{width:'150px'}">名次</text>
@@ -91,11 +95,11 @@
                     </header>
                     <cell v-for="(ls,index) in lists" :key="index" >
                         <div class="panel">
-                            <text class="text" :style="{width:'150px'}">{{ls.No}}</text>
+                            <text class="text" :style="{width:'150px'}">{{index+1}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Name}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Quantity}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Amount}}</text>
-                            <text class="text" :style="{width:'150px'}">{{ls.LvStr}}</text>
+                            <text class="text" :style="{width:'150px'}">{{ls.AmtLVStr}}</text>
                         </div>
                     </cell>
 
@@ -106,10 +110,15 @@
                     </loading>
 
                 </list>
+
+                <div v-if="noDataFlag" style="justify-content: center;align-items: center;height: 100">
+                    <text style="color: #000000;font-size: 30">暂无数据！</text>
+                </div>
+
             </div>
             <!-- 厂商-->
             <div  :style="{'visibility':selectIndex==3?'visible':'hidden'}"  style="position: absolute;left: 20;top: 10;right: 10;bottom: 0">
-                <list style="flex: 1" >
+                <list style="flex: 1" v-if="!noDataFlag">
                     <header>
                         <div class="banner">
                             <text class="txttitle" :style="{width:'150px'}">名次</text>
@@ -121,11 +130,11 @@
                     </header>
                     <cell v-for="(ls,index) in lists" :key="index" >
                         <div class="panel">
-                            <text class="text" :style="{width:'150px'}">{{ls.No}}</text>
+                            <text class="text" :style="{width:'150px'}">{{index+1}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Name}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Quantity}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Amount}}</text>
-                            <text class="text" :style="{width:'150px'}">{{ls.LvStr}}</text>
+                            <text class="text" :style="{width:'150px'}">{{ls.AmtLVStr}}</text>
                         </div>
                     </cell>
 
@@ -137,10 +146,15 @@
 
 
                 </list>
+
+                <div v-if="noDataFlag" style="justify-content: center;align-items: center;height: 100">
+                    <text style="color: #000000;font-size: 30">暂无数据！</text>
+                </div>
+
             </div>
             <!-- 导购-->
             <div  :style="{'visibility':selectIndex==4?'visible':'hidden'}"  style="position: absolute;left: 20;top: 10;right: 10;bottom: 0">
-                <list style="flex: 1" >
+                <list style="flex: 1" v-if="!noDataFlag">
                     <header>
                         <div class="banner">
                             <text class="txttitle" :style="{width:'187px'}">名次</text>
@@ -151,7 +165,7 @@
                     </header>
                     <cell v-for="(ls,index) in lists" :key="index" >
                         <div class="panel">
-                            <text class="text" :style="{width:'187px'}">{{ls.No}}</text>
+                            <text class="text" :style="{width:'187px'}">{{index+1}}</text>
                             <text class="text" :style="{width:'187px'}">{{ls.Name}}</text>
                             <text class="text" :style="{width:'187px'}">{{ls.Quantity}}</text>
                             <text class="text" :style="{width:'187px'}">{{ls.Amount}}</text>
@@ -166,29 +180,47 @@
                     </loading>
 
                 </list>
+
+                <div v-if="noDataFlag" style="justify-content: center;align-items: center;height: 100">
+                    <text style="color: #000000;font-size: 30">暂无数据！</text>
+                </div>
+
             </div>
             <!-- 货品 销售排行-->
             <div  :style="{'visibility':selectIndex==5?'visible':'hidden'}"  style="position: absolute;left: 20;top: 10;right: 10;bottom: 0">
-                <list style="flex: 1" >
+                <list style="flex: 1" v-if="!noDataFlag">
                     <header>
                         <div class="banner">
                             <text class="txttitle" :style="{width:'150px'}">名次</text>
                             <text class="txttitle" :style="{width:'150px'}">货号</text>
                             <text class="txttitle" :style="{width:'150px'}">数量</text>
-                            <text class="txttitle" :style="{width:'150px'}">零售价</text>
+                            <text class="txttitle" :style="{width:'150px'}">金额</text>
                             <text class="txttitle" :style="{width:'150px'}">平均售价</text>
                         </div>
                     </header>
                     <cell v-for="(ls,index) in lists" :key="index" >
                         <div class="panel">
-                            <text class="text" :style="{width:'150px'}">{{ls.No}}</text>
+                            <text class="text" :style="{width:'150px'}">{{index+1}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Name}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Quantity}}</text>
                             <text class="text" :style="{width:'150px'}">{{ls.Amount}}</text>
-                            <text class="text" :style="{width:'150px'}">{{ls.AvgPrice}}</text>
+                            <text class="text" :style="{width:'150px'}" >{{ls.AvgPrice}}</text>
                         </div>
                     </cell>
+
+                    <!--  用于给列表添加上拉加载更多的功能-->
+                    <loading class="loading" @loading="onloading" :display="loadinging ? 'show' : 'hide'">
+                        <loading-indicator class="indicator"></loading-indicator>
+                        <text class="indicator-text">Loading...</text>
+                    </loading>
+
                 </list>
+
+                <div v-if="noDataFlag" style="justify-content: center;align-items: center;height: 100">
+                    <text style="color: #000000;font-size: 30">暂无数据！</text>
+                </div>
+
+
             </div>
 
 
@@ -223,6 +255,7 @@
     import {Utils} from 'weex-ui'
     import module1 from './jstools/mytool'
     var nav = weex.requireModule('navigator')
+    const progress = weex.requireModule('progress');
     const  pref=weex.requireModule('pref')
     const net = weex.requireModule('net');
     var pstatic=weex.requireModule("static")
@@ -262,28 +295,32 @@
 
                 this.searchType =p.hasOwnProperty('searchType')?p.searchType:'Department'
                 this.log('searchType:'+this.searchType)
-                if(this.searchType =='Department') {
+
                     param.searchType =this.searchType
                     param.BeginDate = this.BeginDate
                     param.EndDate = this.EndDate
-                    param.Condition = ''//这里之前是，年，月，日，但这里都是给beginddate
-                    param.DisType = '汇总'
-                    param.DepartmentID = ''
-                    param.DistrictID = ''
+                    param.Condition = p.hasOwnProperty('Condition')?p.Condition:''//这里之前是，年，月，日，但这里都是给beginddate
+                    param.DisType = '明细'//汇总
+                    param.DepartmentID = p.hasOwnProperty('DepartmentID')?p.DepartmentID:''
+                    param.DistrictID = p.hasOwnProperty('DistrictID')?p.DistrictID:''
                     param.Orderby = '金额'
-                }
+
                 pstatic.set('report',param)
                 net.post(pref.getString('ip')+'/salesTicket.do?report',param,{},function(){
                     //start
+                    progress.showFull('正在载入',false)
                 },function(e){
                     //success self.back=e.res;
                     that.lists.splice(0,that.lists.length)//因为共用这个
                     that.totallist.splice(0,that.totallist.length)//因为共用这个
                     that.currPage =1
+                    that.totalQty =0
+                    that.totalAmt =0
                     if(e.res.success){
                         that.totallist=e.res.obj || []
                         if(that.totallist.length==0){
                            that.noDataFlag =true
+                            that.total()
                         }else{
                             that.noDataFlag= false
                             that.log('that.currPage：'+that.currPage)
@@ -295,12 +332,14 @@
                     }else {
                         that.toast(e.res.msg)
                     }
+                    progress.dismiss()
 
                 },function(e){
                     //compelete
 
                 },function(){
                     //exception
+                    progress.dismiss()
                 });
 
             },modifymenu(){
@@ -371,6 +410,7 @@
                     this.searchType ='Department'
                 }else if(this.selectIndex ==1){
                     this.searchType='GoodsType'
+                    this.log('searchType=1'+this.searchType)
                 }else if(this.selectIndex ==2){
                     this.searchType='Brand'
                 }else if(this.selectIndex ==3){
@@ -381,6 +421,7 @@
                     this.searchType='Goods'
                 }
                 p.searchType =this.searchType
+                p.DepartmentID ='' //切换就要马上清空
                 this.log('汇总项目的触发report:'+JSON.stringify(p))
                 pstatic.set('report',p)
                 this.onLoad(p)
@@ -509,18 +550,22 @@
                         this.condition =''
                          var arr = e.item || []
                         if(arr.length ==1){
-                            this.condition=arr[0].id //不用，号
+                            this.condition="'"+arr[0].id+"'" //不用，号
                         }else if(arr.length >1) {
                             for (var i = 0; i < arr.length; i++) {
-                                this.condition = this.condition + "'" + (arr[i].id) + ("',")
+                                if(i==arr.length-1) {
+                                    this.condition = this.condition + "'" + (arr[i].id) + ("'")
+
+                                }else{
+                                    this.condition = this.condition + "'" + (arr[i].id) + ("',")
+                                }
                             }
                         }
-                         if(this.condition.indexOf(',') !=-1){
-                             this.condition=this.condition.substring(0,this.condition.length-1)
-                         }
+
                          this.log('condition列表字符串:'+this.condition)//this.condition.join() 如果是列表可以这样用
                         p1.DepartmentID =this.condition
                         pstatic.set('report',p1)
+                        this.log('p1:'+JSON.stringify(p1))
                         this.onLoad(p1)
                     }
 
